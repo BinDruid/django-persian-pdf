e2bcace76905368543c129e3097339a5df9ca1a3
BinDruid <abharya.dev@gmail.com>
2024-04-19 12:34:40 +0330

    Update app name

--- src/django_persian_pdf/apps.py
+++ src/django_persian_pdf/apps.py
@@ -3,6 +3,6 @@ from __future__ import annotations
 from django.apps import AppConfig
 
 
-class CachePydanticAppConfig(AppConfig):
+class DjangoPersianPDF(AppConfig):
     name = "django_persian_pdf"
     verbose_name = "django-persian-pdf"
